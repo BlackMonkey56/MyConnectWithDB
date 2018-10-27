b7e6e81661517a2cf06100f61ae60aefbb162e9c
Lee <blackmonkey9256@gmail.com>
2018-10-28 01:29:13 +0900

    Add URLConnector field on MainActivity

--- app/src/main/java/com/example/machina/myconnectwithdb/MainActivity.java
+++ app/src/main/java/com/example/machina/myconnectwithdb/MainActivity.java
@@ -4,10 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String test;
+    private URLConnector task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = "https://localhost/";
+        task = new URLConnector(test);
+
+        task.start();
+
+        try{
+            task.join();
+            System.out.println("Waiting for result...");
+        }catch (InterruptedException e){
+
+        }
+
+        String result = task.getResult();
+
+        System.out.println(result);
     }
 }
