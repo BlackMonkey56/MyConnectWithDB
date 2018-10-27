f9cb29822bb8d3562f3b88bb4fb186202120737c
Lee <blackmonkey9256@gmail.com>
2018-10-28 03:53:51 +0900

    Add parseJSON method

--- app/src/main/java/com/example/machina/myconnectwithdb/MainActivity.java
+++ app/src/main/java/com/example/machina/myconnectwithdb/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.machina.myconnectwithdb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     private String test;
     private URLConnector task;
+    private JSONObject jo = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +30,28 @@ public class MainActivity extends AppCompatActivity {
         String result = task.getResult();
 
         System.out.println(result);
+
+        parseJSON(result);
+    }
+
+    public String parseJSON(String target){
+        try{
+            JSONObject jo = new JSONObject(target);
+
+            //Get data by 'name'
+            JSONArray jsonArr = jo.getJSONArray("id");
+
+            for(int i = 0; i < jsonArr.length(); i++){
+                JSONObject object = jsonArr.getJSONObject(i);
+
+                System.out.println(object.getString("id"));
+            }
+
+            return "";
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
